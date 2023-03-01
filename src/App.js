1d2f7ee773a864a15253d3113e4966fb29acdb79
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2023-03-01 23:08:55 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-
-//pages
-import Home from "./pages/Home";
-import LoremIpsum from "./pages/LoremIpsum";
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Nav />}>
-          <Route index element={<Home />} />
-          <Route path="/lorem" element={<LoremIpsum />} />
-        </Route>
-      </Routes>
+      <Navbar />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/cart" component={Cart} />
+        {/* <Route exact path="/products/:id" component={Product} /> */}
+      </Switch>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./utils/Layout";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

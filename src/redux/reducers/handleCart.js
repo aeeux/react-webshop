f45c27c67c3f391c42dc85f1fe00d8f7f560dbf9
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2023-03-07 22:02:17 +0100

    Update handleCart.js

--- src/redux/reducers/handleCart.js
+++ src/redux/reducers/handleCart.js
@@ -2,10 +2,10 @@ const cart = [];
 
 const handleCart = (state = cart, action) => {
   const product = action.payload;
+  const exist = state.find((x) => x.id === product.id);
+
   switch (action.type) {
     case "ADDITEM":
-      // Check if Product already exists
-      const exist = state.find((x) => x.id === product.id);
       if (exist) {
         // Increase the quantity
         return state.map((x) =>
@@ -24,9 +24,8 @@ const handleCart = (state = cart, action) => {
       break;
 
     case "DELITEM":
-      const exist1 = state.find((x) => x.id === product.id);
-      if (exist1.qty === 1) {
-        return state.filter((x) => x.id !== exist1.id);
+      if (exist.qty === 1) {
+        return state.filter((x) => x.id !== exist.id);
       } else {
         return state.map((x) =>
           x.id === product.id ? { ...x, qty: x.qty - 1 } : x

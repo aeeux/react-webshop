1b81f7935aec04fa6871073f36146f21f0280917
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2023-03-09 19:13:39 +0100

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -131,12 +131,6 @@ const Products = () => {
   return (
     <div>
       <div className="container my-5 py-5">
-        <div className="row">
-          <div className="col-12 mb-5">
-            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
-            <hr />
-          </div>
-        </div>
         <div className="row justify-content-center">
           {loading ? <Loading /> : <ShowProducts />}
         </div>
